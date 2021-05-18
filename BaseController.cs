84376613ab14f29f3a38a0a6057471fae3f9fe58
Navya5855 <72745797+Navya5855@users.noreply.github.com>
2021-05-18 18:31:25 +0530

    Add files via upload
    commit changes updated

---
+++ BaseController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TestServices.Controllers
+{
+    [Produces("application/json")]
+    [Route("api/[controller]")]
+    [ApiController]
+    [Authorize(AuthenticationSchemes = Microsoft.AspNetCore.Authentication.JwtBearer.JwtBearerDefaults.AuthenticationScheme)]
+    public class BaseController : ControllerBase
+    {
+
+       
+    }
+}
